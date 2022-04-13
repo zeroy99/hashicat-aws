81b92d0257e939068ac5a49498e9a14edda82f0c
Yohan Riou <yriou@boylesports.com>
2022-04-13 12:51:54 +0000

    Added s3 bucket module #2

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/hashicat-yr/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
-  bucket_prefix= var.prefix
+  bucket_prefix = var.prefix
 }
